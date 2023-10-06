4daefa941e7887068d06a1868f913a1c7c901f66
ankitatra <ankitathakurankita7007@gmail.com>
2023-10-06 17:57:37 +0530

    ADD

--- model/fetures.model.js
+++ model/fetures.model.js
@@ -21,4 +21,30 @@ const featuresSchema = new mongoose.Schema({
 
 const Features = mongoose.model("Features", featuresSchema);
 
-module.exports =  Features ;
+
+const adds= new mongoose.Schema({
+  Image : {
+    type: String,
+    required: true,
+  },
+  alttag:{
+    type:String,
+    required:true
+  },
+  adTittle : {
+    type: String,
+    required: true,
+  },
+  Discription:{
+    type:String,
+    required: true,
+  },
+ redirectionalURL:{
+    type:String,
+    required: true,
+  }
+});
+
+const Adds = mongoose.model("Adds", adds);
+
+module.exports =  {Features ,Adds}
--- routes/features.routes.js
+++ routes/features.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const features = express.Router();
-const Features = require("../model/fetures.model"); // Import your Category model
+const {Features, Adds} = require("../model/fetures.model"); // Import your Category model
+const adminauth = require("../middleware/adminauth.middleware");
 
 features.post("/add", async (req, res) => {
   try {
@@ -64,5 +65,72 @@ features.delete("/delete/:id", async (req, res) => {
     res.status(500).json({ error: "An error occurred while deleting the category." });
   }
 });
+
+
+
+features.post('/add/adds',adminauth,async (req, res) => {
+  try {
+    const adds=await Adds.find()
+    if(adds.length==2){
+      return res.status(400).json({ error: 'adds limit exceeds' });
+    }
+    const description = req.body.Discription;
+    const words = description.split(/\s+/);
+
+    if (words.length > 15) {
+      return res.status(400).json({ error: 'Description exceeds the word limit of 15 words' });
+    }
+
+    const newAdd = new Adds({
+      Image: req.body.Image,
+      alttag: req.body.alttag,
+      adTittle: req.body.adTittle,
+      Discription: req.body.Discription,
+      redirectionalURL: req.body.redirectionalURL,
+    });
+   const savedAdd = await newAdd.save();
+    res.status(201).json(savedAdd);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+features .get("/adds/", async (req, res) => {
+  try {
+    const adds = await Adds.find();
+    res.status(200).json(adds);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred while fetching categories." });
+  }
+});
+
+features.patch("/adds/update/:id",adminauth, async (req, res) => {
+  const id = req.params.id;
+  try {
+    const updatedCategory = await Adds.findByIdAndUpdate(id, req.body, { new: true });
+    if (!updatedCategory) {
+      return res.status(404).json({ error: "Category not found." });
+    }
+    res.status(200).json(updatedCategory);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred while updating the category." });
+  }
+});
+
+features.delete('/adds/delete/:id',adminauth,async (req, res) => {
+  const id = req.params.id;
+  try {
+    const existingAdd = await Adds.findByIdAndRemove(id);
+    if (!existingAdd) {
+      return res.status(404).json({ error: 'Add not found' });
+    }
+    res.status(204).send(); // No content response for successful deletion
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 
 module.exports = features;
